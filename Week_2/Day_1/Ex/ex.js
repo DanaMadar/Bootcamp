1ae042d0930bc77919442d3fd92aef5a6c4e87ce
DanaMadar <dana.madar@gmail.com>
2021-10-10 13:24:17 +0300

    Th

---
+++ Week_2/Day_1/Ex/ex.js
@@ -0,0 +1,13 @@
+console.log(5 + "34");
+console.log(5 - "4");
+console.log(10 % 5);
+console.log(5 % 10);
+console.log("Java" + "Script");
+console.log(" " + " ");
+console.log(" " + 0);
+console.log(true + true);
+console.log(true + false);
+console.log(false + true);
+console.log(false - true);
+console.log(3 - 4);
+console.log("Bob" - "bill")
---
+++ Week_2/Day_1/js-basics/script.js
@@ -0,0 +1,10 @@
+let addressNumber = 25;
+let addressStreet = "Yud Bet St.";
+let country = "Israel";
+
+let global_address = `I live at ${addressStreet} ${addressNumber} in ${country}`
+console.log(global_address)
+
+addressNumber = addressNumber + 1;
+addressNumber += 1;
+addressNumber ++;
