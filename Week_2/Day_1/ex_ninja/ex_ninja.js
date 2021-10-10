f14d9f94e9213c2738552dfa1267dd6abaad43c0
DanaMadar <dana.madar@gmail.com>
2021-10-10 15:30:32 +0300

    boom

--- Week_2/Day_1/ex_ninja/ex_ninja.js
+++ Week_2/Day_1/ex_ninja/ex_ninja.js
@@ -1,6 +1,20 @@
 //ex_1
-
 sentence_user = prompt("Write a sentence inclueding 'Nemo'");
 sentence_user2 = sentence_user.indexOf("Nemo");
 
 console.log("I found Nemo at position ", sentence_user2)
+
+//ex4
+let explosion = "boooooooooom";
+let end = 0;
+
+count = explosion.split("o").length - 1;
+if (count % 2 == 0){
+    end = explosion + "!"
+} 
+if (count % 5 == 0){
+    end = end.toUpperCase()
+}
+alert(end)
+
+
