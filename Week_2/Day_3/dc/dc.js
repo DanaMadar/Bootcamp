d70b2990f64b0ab8490826d5037b0ef4d4d94783
DanaMadar <dana.madar@gmail.com>
2021-10-12 19:17:45 +0300

    q

--- Week_2/Day_3/dc/dc.js
+++ Week_2/Day_3/dc/dc.js
@@ -1,8 +1,18 @@
 let star = "";
-let count = 0;
+let count = 3;
+
 
 while (count< 6) {
     star +="*";
     count += 1;
     console.log(star);
+}
+
+
+
+let star1 = [["*", "**"], ["***","****"], ["*****","******"]];
+for (var i=0; i < star1.length; i++) {
+ for (var j=0; j < star1[i].length; j++) {
+   console.log(star1[i][j]);
+ }
 }
