e3e97f818dc9428c8f791e45b0246c91395ec1ea
DanaMadar <dana.madar@gmail.com>
2021-10-12 20:01:59 +0300

    t

--- Week_2/Day_3/ex/ex.js
+++ Week_2/Day_3/ex/ex.js
@@ -1,41 +1,41 @@
 //Ex1
-// let colors = ["blue", "yello", "red", "purple"];
+let colors = ["blue", "yello", "red", "purple"];
 
-// for (let index in colors) {
-//     console.log(colors[index])
-//  }
+for (let index in colors) {
+    console.log(colors[index])
+ }
 
-// for (let i of colors) {
-//     console.log(i);
-// }
+for (let i of colors) {
+    console.log(i);
+}
 
 
  //Ex2
-// let people = ["Greg", "Mary", "Devon", "James"];
-// let peopleNew;
-// people.shift();
-// people.splice(2, 1,"Jason");
-// people.push("Dana");
+let people = ["Greg", "Mary", "Devon", "James"];
+let peopleNew;
+people.shift();
+people.splice(2, 1,"Jason");
+people.push("Dana");
 
-// for (let i of people) {
-//     console.log(i);
-// }
+for (let i of people) {
+    console.log(i);
+}
 
-// peopleNew = people.slice(1);
-// console.log(peopleNew.indexOf("Mary"));
-// console.log(peopleNew.indexOf("Foo"));
+peopleNew = people.slice(1);
+console.log(peopleNew.indexOf("Mary"));
+console.log(peopleNew.indexOf("Foo"));
 
-// let last = peopleNew[peopleNew.length -1];
-// console.log(last);
+let last = peopleNew[peopleNew.length -1];
+console.log(last);
 
 
-// //Ex3
- //let userNumber = parseInt(prompt("Type in any number"));
+//Ex3
+ let userNumber = parseInt(prompt("Type in any number"));
 
-// while (userNumber < 10) {
-//     userNumber = parseInt(prompt("Type in another number"));
-// }
-// console.log(userNumber);
+while (userNumber < 10) {
+    userNumber = parseInt(prompt("Type in another number"));
+}
+console.log(userNumber);
 
 //Ex4
 let guestList = {
@@ -46,32 +46,32 @@ let guestList = {
     sam: "Argentina"
   }
 
-let guestName = prompt("Whats your name").toLowerCase();
 
-for (item in guestList){
-  console.log(item);
+const student = prompt('please enter your name:').toLowerCase();
+const keys = Object.keys(guestList), index = keys.indexOf(student);
+console.log(index === -1 ? `Hi! I'm a guest.` :
+  `Hi! I'm ${keys[index]}, and I'm from ${guestList[student]}.`);
+
+  // Ex5
+let x = {family: "Paris"};
+
+
+for (let element in x) {
+  console.log(x);
 }
-
-//Ex5
-// let x = {family: "Paris"};
-
-
-// for (let element in x) {
-//   console.log(x);
-// }
-// for (let element in x) {
-//   console.log(x[element]);
-// }
+for (let element in x) {
+  console.log(x[element]);
+}
 
 
 //Ex6
-// let details = {
-//   my: 'name',
-//   is: 'Rudolf',
-//   the: 'raindeer'
-// }
-// //details = details.replace(/[^0-9\.]+/g, "");
-// let arr = Object.entries(details);
-// let detail1 = arr.join(",");
-// detail1 = detail1.replaceAll(",", " ");
-// console.log(detail1);
+let details = {
+  my: 'name',
+  is: 'Rudolf',
+  the: 'raindeer'
+}
+
+let arr = Object.entries(details);
+let detail1 = arr.join(",");
+detail1 = detail1.replaceAll(",", " ");
+console.log(detail1);
---
+++ Week_2/Day_3/gold3/gold3.js
@@ -0,0 +1,21 @@
+let building = {
+    numberLevels : 4,
+    numberOfAptByLevel : {
+        "1": 3,
+        "2": 4,
+        "3": 9,
+        "4": 2,
+    },
+    nameOfTenants : ["Sarah", "Dan", "David"],
+    numberOfRoomsAndRent:  {
+        "Sarah": [3, 990],
+        "Dan":  [4, 1000],
+        "David": [1, 500],
+    },
+}
+coun= 0;
+
+while (count< 6) {
+    count += 1;
+    console.log(building[numberLevels]);
+}
