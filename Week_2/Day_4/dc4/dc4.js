b13edc88db96fefda181cb7bc5c1ac3e168fea36
DanaMadar <dana.madar@gmail.com>
2021-10-13 20:00:21 +0300

    +

--- Week_2/Day_4/dc4/dc4.js
+++ Week_2/Day_4/dc4/dc4.js
@@ -1,4 +1,10 @@
-userWords = prompt("Write a few words with commas in between");
-arr = userWords.split(",");
-console.log(arr);
+const input = prompt('Please enter a list of words separated by commas');
+const list = input.split(',').map(e => e.trim());
+let longest = 0;
+list.forEach(e => {
+    if (e.length > longest) longest = e.length;
+})
 
+console.log('*'.repeat(longest + 4));
+list.forEach(e => console.log(`* ${e}${' '.repeat(longest - e.length)} *`))
+console.log('*'.repeat(longest + 4));
