afcc00b576e5c14a5ccfe396fc44790e01f6b316
DanaMadar <dana.madar@gmail.com>
2021-10-17 21:05:51 +0300

    w

--- Week_3/Day1/dc1/dc1.js
+++ Week_3/Day1/dc1/dc1.js
@@ -1,18 +1,18 @@
-let planets =  "Mercury,Venus,Earth,Mars,Jupiter,Saturn,Uranus,Neptune".split(",");
-
-for (let x of planets){
-    let div = document.createElement("div");
+let planets = 'Mercury,Venus,Earth,Mars,Jupiter,Saturn,Uranus,Neptune'.split(',');
+for (let x in planets) {
+    let div = document.createElement('div');
     div.id = planets[x];
-    div.className = "planets";
+    div.className = "planet";
     div.innerHTML = planets[x];
     document.body.appendChild(div);
+
 }
 
-document.getElementById("Mercury").style.backgroundColor="lightblue"
-document.getElementById("Venus").style.backgroundColor="lightyellow"
-document.getElementById("Earth").style.backgroundColor="lightpink"
-document.getElementById("Mars").style.backgroundColor="lightgreen"
-document.getElementById("Jupiter").style.backgroundColor="lightorange"
-document.getElementById("Saturn").style.backgroundColor="blue"
-document.getElementById("Uranus").style.backgroundColor="yellow"
-document.getElementById("Neptune").style.backgroundColor="red"
+document.getElementById("Mercury").style.backgroundColor="red"
+document.getElementById("Venus").style.backgroundColor="orange"
+document.getElementById("Earth").style.backgroundColor="yellow"
+document.getElementById("Mars").style.backgroundColor="green"
+document.getElementById("Jupiter").style.backgroundColor="blue"
+document.getElementById("Saturn").style.backgroundColor="purple"
+document.getElementById("Uranus").style.backgroundColor="white"
+document.getElementById("Neptune").style.backgroundColor="brown"
