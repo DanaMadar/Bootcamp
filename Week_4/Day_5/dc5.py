bf4d7f6b32c35be8060b5f60702b94e3ee22f6e5
DanaMadar <dana.madar@gmail.com>
2021-10-28 16:28:27 +0300

    e

--- Week_4/Day_5/dc5.py
+++ Week_4/Day_5/dc5.py
@@ -1,1 +1,5 @@
-user_input = (print(sorted(input("type in any words seperated by a coma: ").split(","))))
+#user_input = (print(",".join(sorted(input("type in any words seperated by a coma: ").split(",")))))
+user_input = input("type in any words seperated by a coma: ")
+
+output = [word for word in user_input.split(",")]
+print(",".join(sorted(list(output))))
--- Week_4/Day_5/mini_projcet.py
+++ Week_4/Day_5/mini_projcet.py
@@ -14,29 +14,34 @@ interduction ='''1. The game is played on a grid that’s 3 squares by 3 squares
 print(interduction)
 
 moves = ["-", "-", "-", "-", "-", "-", "-", "-", "-"]
-board = '''
-****************
-* {} | {} | {} *
-* ---|----|--- *
-* {} | {} | {} *
-* ---|----|--- *
-* {} | {} | {} *
-****************
-'''.format(*moves)
-print(board)
+def display_board():
+    board = '''
+    ****************
+    * {} | {} | {} *
+    * ---|----|--- *
+    * {} | {} | {} *
+    * ---|----|--- *
+    * {} | {} | {} *
+    ****************
+    '''.format(*moves)
+    print(board)
 
 player_input = []
-player_input.append(int(input("where do you wanna set your x/o: ")))
+player_input = int(input("where do you wanna set your x/o: "))
+
+if moves != "-":
+    moves.insert(player_input, "x")
+
+while moves == "x" or "o":
+    ("this spot is taken, try again")
+
 print(player_input)
 
-for i, numb in enumerate(moves):
-    if i == player_input[-1]:
-        moves[i] = player_input[-1]
-    
+
 print(moves)
     
 
-
+display_board()
 # play = "x"
 # take_turn = 0
 # let win():
