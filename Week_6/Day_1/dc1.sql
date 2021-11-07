47cd0b7f64d62c17b8d98fc48bbd46a31b950d66
DanaMadar <dana.madar@gmail.com>
2021-11-07 15:21:00 +0200

    k

---
+++ Week_6/Day_1/dc1.sql
@@ -0,0 +1,9 @@
+--insert into actors(first_name,last_name,age,number_of_oscars) 
+--values('Angelina', 'Jolie', '1975-06-04', '1'), 
+--('Georg', 'Clooney', '1961-06-05', '2'), 
+--('Jennifer', 'Aniston', '1969-02-11', 0),
+--('Matt', 'Damon', '1970-08-10', 5)
+
+--SELECT COUNT(*)FROM actors;
+--insert into actors(first_name,last_name,age,number_of_oscars) 
+--values ('Gal', 'Gadot', '', 100000)
--- Week_6/Day_1/file.sql
+++
@@ -1,1 +0,0 @@
-SELECT * FROM costumers WHERE f_name != 'Scott';
