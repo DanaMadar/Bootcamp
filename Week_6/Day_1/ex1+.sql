5cd2f3606ec54f0fed4323607419e652fa24ded8
DanaMadar <dana.madar@gmail.com>
2021-11-08 14:56:24 +0200

    d

---
+++ Week_6/Day_1/ex1+.sql
@@ -0,0 +1,33 @@
+--create database bootcamp;
+--create table students(
+--id int(5) primary key AUTO_INCREMENT,
+--last_name varchar(100),
+--first_name varchar(100),
+--birth_date date
+--);
+
+--insert into students(last_name,first_name,birth_date)
+--values
+--('Marc',	'Benichou',	'2010-10-02'),
+--('Cohen',	'Yoan',	'2010-12-03'),
+--('Lea',	'Benichou',	'1987-07-27'),
+--('Amelia',	'Dux',	'1996-04-07') ,
+--('David',	'Grez',	'2003-06-14') ,
+--('Aurelie',	'Laura',	'2003-06-13') ,
+--('Omer',	'Simpson',	'1980-10-03');
+
+--select * from students;
+--select first_name,last_name from students;
+--select first_name,last_name from students;
+--select first_name,last_name from students where id=2;
+--select * from students where last_name="Benichou" and first_name="Marc";
+--select * from students where last_name="Benichou" or first_name="Marc";
+--select * from students where first_name like "%a%";
+--select * from students where first_name like 'a%';
+--select * from students where first_name like '%a';
+--select * from students where first_name like '%a_';
+--select first_name from students where first_name like '%a-';
+--select * from students where id=1 or id=3;
+--select * from students where birth_date>='2000-01-01';
+--insert into students(id, last_name, first_name, birth_date)
+--values (29,'joan','marc','2021-01-21')
--- Week_6/Day_1/ex1.sql
+++ Week_6/Day_1/ex1.sql
@@ -1,1 +1,32 @@
-SELECT * FROM costumers WHERE f_name != 'Scott';
+--create table items(
+--name varchar(100),
+--price int(5)
+--);
+
+--insert into items( name,price)
+                  values("Small Desk", 100),
+                  ("Large Desk", 300),
+                  ("Fan", 80),
+                  ("Ball", 40),
+                  ("Banana", 20);
+
+--create table customers(
+    --first_name varchar(100),
+    --last_name varchar(100)
+--);
+
+--insert into customers(first_name, last_name)
+--VALUES ("Greg", "Jones"),
+--("Sandra", "Jones"),
+--("Scott", "Scott"),
+--("Trevor", "Green"),
+--("John", "Smith"),
+--("Melanie", "Johnson");
+
+--select * from items;
+--select * from items where price < 80;
+--select * from items where price < 300;
+--select * from customers where last_name="Smith" ;
+--select * from customers where last_name="Jones" ;
+--select * from customers where first_name="Scott" ;
+
